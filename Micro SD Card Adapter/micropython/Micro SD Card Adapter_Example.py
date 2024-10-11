e642fbf44c9d4d0702fe29041a31aa0de967fae0
Meijersson <marshall.d.ilyas@gmail.com>
2024-10-11 12:40:10 +0200

    micro sd card example modified

--- Micro SD Card Adapter/micropython/Micro SD Card Adapter_Example.py
+++ Micro SD Card Adapter/micropython/Micro SD Card Adapter_Example.py
@@ -1,53 +1,24 @@
 import machine
-import os
+import uos
 import sdcard
-import time
 
-# Setup SPI interface (using SPI0)
-spi = machine.SPI(0, baudrate=1000000, sck=machine.Pin(18), mosi=machine.Pin(19), miso=machine.Pin(16))
+cs_pin = machine.Pin(17, machine.Pin.OUT)
+spi = machine.SPI(0, baudrate=1000000, polarity=0, phase=0,bits=8, firstbit=machine.SPI.MSB,
+                  sck=machine.Pin(18), mosi=machine.Pin(19), miso=machine.Pin(16))
 
-# Chip Select (CS) pin
-cs = machine.Pin(17, machine.Pin.OUT)
+sd = sdcard.SDCard(spi, cs_pin)
 
-# Initialize SD card
-try:
-    sd = sdcard.SDCard(spi, cs)  # Try initializing the SD card
-    print("SD Card initialized successfully.")
-except Exception as e:
-    print("Failed to initialize SD card:", e)
-    raise
+vfs = uos.VfsFat(sd)
+uos.mount(vfs, "/sd")
+fn = "/sd/sdtest.txt"
 
-# Mount the filesystem
-try:
-    vfs = os.VfsFat(sd)
-    os.mount(vfs, "/sd")
-    print("Filesystem mounted successfully.")
-except Exception as e:
-    print("Failed to mount filesystem:", e)
-    raise
-
-# List files in the root directory
-print("Files on SD card:", os.listdir("/sd"))
-
-# Create a file and write data
-try:
-    with open("/sd/hello.txt", "w") as file:
-        file.write("Hello, MicroSD card from Raspberry Pi Pico!")
-        print("Data written to hello.txt successfully.")
-except Exception as e:
-    print("Failed to write to SD card:", e)
-
-# Read the content of the file
-try:
-    with open("/sd/hello.txt", "r") as file:
-        content = file.read()
-        print("File content:", content)
-except Exception as e:
-    print("Failed to read from SD card:", e)
-
-# Unmount the filesystem
-try:
-    os.umount("/sd")
-    print("SD card unmounted successfully.")
-except Exception as e:
-    print("Failed to unmount SD card:", e)
+with open(fn, "w") as f:
+    print("Writing data to file sdtest.txt...")
+    f.write("This is a test for micro SD card\r\n")
+    print("Writing to file completed")
+    
+with open(fn, "r") as f:
+    print("Reading data from file sdtest.txt...")
+    data = f.read()
+    print("Data read completed")
+    print("Data:",data)
