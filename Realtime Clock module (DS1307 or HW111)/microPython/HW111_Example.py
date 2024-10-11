566fc3b450c03a056e88e853c9f4df8e735d1583
HighW0rks <127955603+HighW0rks@users.noreply.github.com>
2024-10-11 12:23:23 +0200

    Updated code
    Removed the # from the initialize of the time on a HW111 or DS1307. Removed 2 functions that weren't necessary.

--- Realtime Clock module (DS1307 or HW111)/microPython/HW111_Example.py
+++ Realtime Clock module (DS1307 or HW111)/microPython/HW111_Example.py
@@ -9,7 +9,7 @@ i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=100000)
 rtc = HW111(i2c)
 
 # Set the current date and time: (year, month, day, weekday, hour, minute, second, subseconds)
-# rtc.datetime((2024, 10, 11, 5, 11, 18, 30, 0))
+rtc.datetime((2024, 10, 11, 5, 11, 18, 30, 0))
 
 # Loop to print the current datetime every second
 while True:
