3030c600aa26cbf2c549f59b3b61a5a11a853dc5
Meijersson <marshall.d.ilyas@gmail.com>
2024-10-25 12:00:01 +0200

    Update SG90_Example.py

--- Motor/Servo Motor (SG90)/microPython/SG90_Example.py
+++ Motor/Servo Motor (SG90)/microPython/SG90_Example.py
@@ -1,9 +1,8 @@
 from machine import Pin, PWM
 from time import sleep
 
-
 servo = PWM(Pin(16))
-servo.freq(50)  
+servo.freq(50)
 
 def set_angle(angle):
     min_duty = 2000   
@@ -12,13 +11,10 @@ def set_angle(angle):
     servo.duty_u16(duty)
 
 while True:
-    # Sweep from 0° to 180°
-    for angle in range(0, 181, 5):
-        set_angle(angle)
-        sleep(0.02)  # 20 ms delay
-    
-    # Sweep from 180° to 0°
-    for angle in range(180, -1, -5):
-        set_angle(angle)
-        sleep(0.02)  # 20 ms delay
+    # Beweeg naar 180 graden
+    set_angle(180)
+    sleep(3)  # Pauzeer voor 3 seconden op 180 graden
 
+    # Beweeg terug naar 0 graden
+    set_angle(0)
+    sleep(3)  # Pauzeer voor 3 seconden op 0 graden (optioneel)
