54c852cf1857a70e0ba7c3c0fa7545bd841e567e
ArjanKam <akamberg@davinci.nl>
2024-10-25 22:45:06 +0200

    update

--- Display/Matrix (Max7219)/microPython/Max7219_Example.py
+++ Display/Matrix (Max7219)/microPython/Max7219_Example.py
@@ -1,47 +1,20 @@
-import sys
-if sys.platform != 'rp2': # raspian pico
-    print(40 * "-")
-    print(">>>>>> Demo for Respberry Pi Pico <<<<<<")
-    print(40 * "-")
-    quit()
-    
-"""
-    about   : Max7219Matrix is IDisplay inherrited class for displaying
-              on a max 7219 matrix connected to a pico
-    Version : 1.0.0
-    Date    : 10 April 2024
-    
-    MAX7219 driver: https://github.com/mcauser/micropython-max7219
-    Note: this driver is designed for 4-in-1 MAX7219 modules.
-"""
-
 from machine import Pin, SPI
-from utime import sleep
 from Max7219 import Matrix8x8
 
-PIXELS_MODULE_WIDTH: int = 8
-PIXELS_MODULE_HEIGHT:int = 8
-PIXELS_HEIGHT = PIXELS_MODULE_WIDTH
-PSI_CHANNEL = 0
-SCROLL_DELAY = 50  # MAX7219 display scrolling speed (ms)
-SLEEP_ANIMATION = .5
-PIXEL_ON = 1
-PIXEL_OFF = 0
+PSI_CHANNEL = 0 # Sets SPI channel to 0
+PIN_SCK = 2     # shows to which pin it needs to be connected on the Raspberry pi pico
+PIN_MOSI = 3    # shows to which pin it needs to be connected on the Raspberry pi pico
+PIN_CS = 5      # shows to which pin it needs to be connected on the Raspberry pi pico
 
-_pin_sck = 2
-_pin_mosi = 3
-_pin_cs = 5
-_brightness = 4
-number_of_modules_x = 4
-number_of_modules_y = 1
-_width  = number_of_modules_x * PIXELS_MODULE_WIDTH
-_height = number_of_modules_y * PIXELS_MODULE_HEIGHT
+BRIGHTNESS = 4  		 # Select the brightness of the screen(ranges from 0-15)
+number_of_modules_x = 4  # The number of Screens connected in the X access(width)
+number_of_modules_y = 1  # The number of screens conencted in the Y access(height)
 
-spi = SPI(PSI_CHANNEL, sck=Pin(_pin_sck), mosi=Pin(_pin_mosi))
-cs = Pin(_pin_cs, Pin.OUT)
+spi = SPI(PSI_CHANNEL, sck=Pin(PIN_SCK), mosi=Pin(PIN_MOSI))
+cs = Pin(PIN_CS, Pin.OUT)
+
 display = Matrix8x8(spi, cs, number_of_modules_x * number_of_modules_y)
-display.brightness(_brightness)
+display.brightness(BRIGHTNESS)
 
-display.text("hoi", 2, 1)
-display.show()
-
+display.text("hi!!", 2, 1)  # Enter the text you wish to display
+display.show()  			# This actualy shows the text on the display
