cd2d5d9509623aa312804000f9f32a9a7b7da9b2
ArjanKam <akamberg@davinci.nl>
2024-10-25 23:02:39 +0200

    files

--- Switches/RFID Module (RC522)/microPython/RC522_Example.py
+++ Switches/RFID Module (RC522)/microPython/RC522_Example.py
@@ -1,17 +1,22 @@
 from rc522 import RC522
 import utime
 
-reader = RC522(spi_id=0, sck=6, miso=4, mosi=7, cs=5, rst=22)
+reader = RC522(spi_id=0, sck=6, miso=4, mosi=7, cs=5, rst=3)
+reader.init()
 
 print("Bring TAG closer...")
 print("")
 
 while True:
-    reader.init()
-    (stat, tag_type) = reader.request(reader.REQIDL)
+    utime.sleep_ms(500)
+    stat, tag_type = reader.request(reader.REQIDL)
+    
+    if stat != reader.OK:
+        continue
+        
+    stat, uid = reader.SelectTagSN()
     if stat == reader.OK:
-        (stat, uid) = reader.SelectTagSN()
-        if stat == reader.OK:
-            card = int.from_bytes(bytes(uid), "little", False)
-            print("CARD ID: " + str(card))
-utime.sleep_ms(500)
+        card = int.from_bytes(bytes(uid), "little", False)
+        print(f"CARD ID: {card}")
+            
+    
