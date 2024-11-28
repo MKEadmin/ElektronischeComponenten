372eaeeac795621c36ff8d0679535b11dbba5d3f
ArjanKam <akamberg@davinci.nl>
2024-11-28 10:15:05 +0100

    Update BME280_Example.py

--- Environment/Temperature Humidity Pressure (BME280)/microPython/BME280_Example.py
+++ Environment/Temperature Humidity Pressure (BME280)/microPython/BME280_Example.py
@@ -1,31 +1,21 @@
 # Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-bme280-micropython/
-
 from machine import Pin, I2C
 from time import sleep
-import BME280
+from BME280 import BME280
 
 # Initialize I2C communication
 i2c = I2C(id=0, scl=Pin(1), sda=Pin(0), freq=40000)
 
+# Initialize BME280 sensor
+bme = BME280(i2c=i2c)
+        
 while True:
     try:
-        # Initialize BME280 sensor
-        bme = BME280.BME280(i2c=i2c)
-        
-        # Read sensor data
-        tempC = bme.temperature
-        hum = bme.read_raw_humidity()
-        pres = bme.pressure
-        
-        # Convert temperature to fahrenheit
-        tempF = (bme.read_temperature()/100) * (9/5) + 32
-        tempF = str(round(tempF, 2)) + 'F'
-        
         # Print sensor readings
-        print('Temperature: ', tempC)
-        print('Temperature: ', tempF)
-        print('Humidity: ', hum)
-        print('Pressure: ', pres)
+        print("--------------------------------------")
+        print(f"Temperature: {bme.getTemperature()[0]:10.2f}")
+        print(f"Humidity   : {bme.getHumidity()[0]:10.2f}")
+        print(f"Pressure   : {bme.getPressure()[0]:10.2f}")
         
     except Exception as e:
         # Handle any exceptions during sensor reading
