b8fbdf94a26ee9a68af6089a30fb9cd3a172badb
ArjanKam <akamberg@davinci.nl>
2024-12-02 13:40:14 +0100

    Update BMI160_Example.py

---
+++ Movement/Gyroscope Accelerometer (BMI160)/microPython/BMI160_Example.py
@@ -0,0 +1,13 @@
+import time
+from machine import Pin, I2C
+from micropython_bmi160 import bmi160
+
+i2c = I2C(1, sda=Pin(2), scl=Pin(3))  # Correct I2C pins for RP2040
+bmi = bmi160.BMI160(i2c)
+
+while True:
+    accx, accy, accz = bmi.acceleration
+    print(f"x:{accx:.2f}m/s2, y:{accy:.2f}m/s2, z{accz:.2f}m/s2")
+    gyrox, gyroy, gyroz = bmi.gyro
+    print(f"x:{gyrox:.2f}°/s, y:{gyroy:.2f}°/s, z{gyroz:.2f}°/s")
+    time.sleep(0.5)
